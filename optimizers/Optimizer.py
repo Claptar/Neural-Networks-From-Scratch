152eb617867d54f923ec916db3145409c84ea2ee
Claptar <binkevich.as@phystech.edu>
2022-01-29 20:04:29 +0300

    Added SGD optimizer

---
+++ optimizers/Optimizer.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class SGD:
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+    def update_params(self, layer):
+        layer.weights -= self.learning_rate * layer.gradW
+        layer.biases -= self.learning_rate * layer.gradb
