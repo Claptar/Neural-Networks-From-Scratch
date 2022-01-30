4dec353f7158190a115070aea4f4a3155969e8f2
Claptar <binkevich.as@phystech.edu>
2022-01-30 14:25:49 +0300

    Changed Dense layer param's initialization

--- layers/Layer.py
+++ layers/Layer.py
@@ -16,8 +16,9 @@ class Sequential:
 
 class Dense:
     def __init__(self, in_features, out_features):
-        self.weights = 0.1 * np.random.randn(in_features, out_features)
-        self.bias = np.zeros((1, out_features))
+        stdv = 1. / np.sqrt(in_features)
+        self.weights = np.random.uniform(-stdv, stdv, size=(in_features, out_features))
+        self.bias = np.random.uniform(-stdv, stdv, size=out_features)
         self.inputs = None
         self.gradW = np.zeros_like(self.weights)
         self.gradb = np.zeros_like(self.bias)
